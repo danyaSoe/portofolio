78343c42b754c4f2b8390b8453a8ae281290ed03
Danya Damaris <18224024@std.stei.itb.ac.id>
2026-06-14 23:56:27 +0700

    test ci/cd

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
-/** @type {import('next').Config} */
-const nextConfig = {
-  // Mengatur output ke mode static export (wajib untuk GitHub Pages)
-  output: 'export', 
-  
-  // Beritahu Next.js kalau kamu menggunakan sub-folder /portofolio
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
   basePath: '/portofolio',
   assetPrefix: '/portofolio/',
-
-  // Opsional: Jika kamu menggunakan komponen <Image /> bawaan Next.js
   images: {
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
